33145dfd2cfb438750b75dce469aae6d67601624
hallazie <hallazie@outlook.com>
2021-07-11 23:36:40 +0800

    --amend

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -7,13 +7,29 @@ public class CameraController : MonoBehaviour
 
     public float movementSpeed;
     public float movementTime;
+    public float normalSpeed;
+    public float fastSpeed;
+    public float rotationAmount;
+    public Vector3 zoomAmount;
+
+    public Transform cameraTransform;
+
     public Vector3 newPosition;
+    public Quaternion newRotation;
+    public Vector3 newZoom;
+
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
+    public Vector3 rotateStartPosition;
+    public Vector3 rotateCurrentPosition;
 
     // Start is called before the first frame update
     void Start()
     {
 
         newPosition = transform.position;
+        newRotation = transform.rotation;
+        newZoom = cameraTransform.localPosition;
 
     }
 
@@ -21,12 +37,70 @@ public class CameraController : MonoBehaviour
     void Update()
     {
 
-        HandleMovementInput(); 
+        HandleMovementInput();
+        HandleMouseInput();
         
+    }
+
+    void HandleMouseInput()
+    {
+     
+        // drag and rotate camera (use middle button aka scroll button)
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if(plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = transform.position + ray.GetPoint(entry);
+            }
+        }
+
+        if (Input.GetMouseButton(2))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+                newPosition = dragStartPosition - dragCurrentPosition;
+            }
+        }
+
+        // drag and move camera (use left mouse button)
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rotateStartPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+            Vector3 difference = rotateCurrentPosition - rotateStartPosition;
+            rotateStartPosition = rotateCurrentPosition;
+            newRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5f));
+        }
+
     }
 
     void HandleMovementInput()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = fastSpeed;
+        }
+        else
+        {
+            movementSpeed = normalSpeed;
+        }
 
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
@@ -45,8 +119,31 @@ public class CameraController : MonoBehaviour
             newPosition += (transform.right * -movementSpeed);
         }
 
-        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        if (Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
 
+        if (Input.GetKey(KeyCode.R))
+        {
+            newZoom += zoomAmount;
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            newZoom -= zoomAmount;
+        }
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            newZoom += Input.mouseScrollDelta.y * zoomAmount * 5;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
     }
 
 }
---
+++ Scripts/Explore/HexGrid.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+
+    public int hexGridSize = 10;
+    private List<Vector2> offsetList = new List<Vector2>{
+        new Vector2(0, 2),
+        new Vector2(2*Mathf.Sqrt(3), 1),
+        new Vector2(2*Mathf.Sqrt(3), -1),
+        new Vector2(0, -2),
+        new Vector2(-2*Mathf.Sqrt(3), -1),
+        new Vector2(-2*Mathf.Sqrt(3), 1)
+    };
+
+    [Range(0, 1)]
+    public float triangleMergeProb = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitHexGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void InitHexGrid()
+    {
+
+    }
+
+    private void InitTriaGrid()
+    {
+        Vertex origin = new Vertex(new Vector2(0, 0));
+
+    }
+
+    private void TraverseHex(Vertex origin, List<Vertex> vertexList, int steps)
+    {
+        if (steps == 0) {
+            return;
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 v1 = new Vector3(0, 1, 0);
+        Vector3 v2 = new Vector3(9, 8, -5);
+        Gizmos.DrawLine(v1, v2);
+    }
+
+}
+
+
+public class Vertex
+{
+
+    public Vector2 coordinate;
+
+    public Vertex(Vector2 coord)
+    {
+        this.coordinate = coord;
+    }
+}
+
+public class GridBasic
+{
+    public List<Vector2> vertexCoordList;
+
+    public GridBasic() { }
+
+    public GridBasic(List<Vector2> vertexCoordList)
+    {
+        this.vertexCoordList = vertexCoordList;
+    }
+
+    public Vector2 GetCenter()
+    {
+        float x = 0.0f; 
+        float y = 0.0f;
+        foreach (Vector2 coord in vertexCoordList)
+        {
+            x += coord.x;
+            y += coord.y;
+        }
+        x /= vertexCoordList.Count;
+        y /= vertexCoordList.Count;
+        Vector2 ret = new Vector2(x, y);
+        return ret;
+    }
+
+}
+
+public class GridQuad : GridBasic
+{
+
+}
+
+public class GridTiad: GridBasic
+{
+
+}
+
+public class HexagonGridCoord
+{
+    /*
+     constrain: x + y + z = 0
+     */
+    public int x;
+    public int y;
+    public int z;
+
+    public Vector2 GetCartesianCoordinates()
+    {
+        float cX = 0.0f;
+        float cY = 0.0f;
+        return new Vector2(cX, cY);
+    }
+}
