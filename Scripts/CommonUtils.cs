0b4b71f99bdff3668454d71cbf23ea6211ba624b
hallazie <hallazie@outlook.com>
2021-07-28 01:23:02 +0800

    --amend

---
+++ Scripts/CommonUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonUtils : MonoBehaviour
+{
+
+    public static float GridVertexEuclideanDistance(GridVertex vertex1, GridVertex vertex2)
+    {
+        float distance = Mathf.Sqrt(Mathf.Pow(vertex1.x - vertex2.x, 2) + Mathf.Pow(vertex1.y - vertex2.y, 2));
+        return distance;
+    }
+
+}
