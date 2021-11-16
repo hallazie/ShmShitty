2794fc0025c4d82cc1244b7b3f629e9dd2f5d548
hallazie <hallazie@outlook.com>
2021-11-16 23:18:26 +0800

    change struct

---
+++ Assets/Scripts/GlobalConst.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalConst
+{
+
+    public static float LAYER_HEIGHT = 1.05f;
+    public static float BASEMENT_HEIGHT = 1.0f;
+    public static float ROOF_HEIGHT = 1.0f;
+
+}
